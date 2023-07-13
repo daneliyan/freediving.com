04b5a07b7954648da801e8d3b975743e8481b844
daneliyan <105802998+daneliyan@users.noreply.github.com>
2023-07-13 17:20:48 +0300

    Ссылки в футере
    Создал и передал две ссылки в футере.

--- wp-content/themes/freediving/footer.php
+++ wp-content/themes/freediving/footer.php
@@ -6,7 +6,14 @@
       <div class="flex flex-col items-center justify-center w-full gap-y-12 border-t border-t-white pt-10 pb-8 lg:pt-12 lg:pb-14 lg:justify-between lg:flex-row lg:gap-x-14 2xl:pt-16 2xl:pb-20">
         <div class="flex items-center gap-x-14 max-lg:justify-between max-lg:w-full">
           <?php if( $logo = get_custom_logo() ){ echo $logo; } ?>
-          <a href="contacts.html" class="inline-flex items-center gap-x-5 text-lg font-normal text-white hover:text-accent lg:text-xl">Contact us <i class="icomoon icon-place text-2xl leading-none font-normal"></i></a>
+          <?php 
+          $footer_contacts_link = get_field('footer_contacts_link', 'option');
+          if( $footer_contacts_link ): 
+              $footer_contacts_link_url = $footer_contacts_link['url'];
+              $footer_contacts_link_title = $footer_contacts_link['title'];
+              ?>
+              <a href="<?php echo esc_url($footer_contacts_link_url) ?>" class="inline-flex items-center gap-x-5 text-lg font-normal text-white hover:text-accent lg:text-xl"><?php echo esc_html($footer_contacts_link_title); ?> <i class="icomoon icon-place text-2xl leading-none font-normal"></i></a>
+          <?php endif; ?>
         </div>
         <?php
           if( have_rows('footer_socials', 'option') ): ?>
@@ -25,7 +32,15 @@
           <?php else :
           endif;
         ?>
-        <a href="#" target="_blank" class="inline-block text-lg text-center leading-none text-white text-opacity-60 hover:text-accent lg:text-xl lg:leading-none">Privacy policy</a>
+        <?php 
+        $footer_privacy_link = get_field('footer_privacy_link', 'option');
+        if( $footer_privacy_link ): 
+            $footer_privacy_link_url = $footer_privacy_link['url'];
+            $footer_privacy_link_title = $footer_privacy_link['title'];
+            $footer_privacy_link_target = $footer_privacy_link['target'] ? $footer_privacy_link['target'] : '_self';
+            ?>
+            <a href="<?php echo esc_url($footer_privacy_link_url) ?>" target="<?php echo esc_attr($footer_privacy_link_target); ?>" class="inline-block text-lg text-center leading-none text-white text-opacity-60 hover:text-accent lg:text-xl lg:leading-none"><?php echo esc_html($footer_privacy_link_title); ?></a>
+        <?php endif; ?>
       </div>
     </div>
   </footer>
--- wp-content/themes/freediving/header.php
+++ wp-content/themes/freediving/header.php
@@ -4,7 +4,6 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <?php wp_head(); ?>
-  <!-- <title>Home Page</title> -->
 </head>
 <body class="relative flex h-full flex-col bg-main overflow-x-hidden">
   <header class="header z-10 absolute top-0 left-0 right-0">
