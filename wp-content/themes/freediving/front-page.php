89b07d677bd7db29c09c195be9deeca99cc7c252
daneliyan <105802998+daneliyan@users.noreply.github.com>
2023-07-14 07:24:01 +0300

    Главная страница: Готов Cta блок
    1. Сделал все основные данные через options page (адрес, номер, эмаил, ссылка на ватсап).
    2. Сделал заголовок и подзаголовок через option page. А на страницах, где заголовок и форма отличаются от основного, просто выведу другое поле.
    3. Подключил плагин Contact Form 7. С помощью него сделал эту форму и закинул шорткод в код. Возможно в будущем вынесу его в поле, и буду передавать в код через админку..

--- wp-content/themes/freediving/front-page.php
+++ wp-content/themes/freediving/front-page.php
@@ -407,22 +407,43 @@ get_header(); ?>
       </div>
     </section>
 
-    <section class="ba z-0 relative bg-cover bg-center pt-28 pb-36" style="background-image: url('./img/cta-bg.jpg');">
+    <section class="ba z-0 relative bg-cover bg-center pt-28 pb-36" style="background-image: url(<?php the_field('cta_bg', 'option'); ?>);">
       <div class="mx-auto container px-3 sm:px-6 lg:px-16">
         <div class="relative flex flex-col justify-between flex-wrap gap-y-10 max-lg:items-center lg:flex-row">
-          <img src="./img/cta-decor.png" class="w-48 max-lg:mb-16 xl:absolute xl:w-80 xl:rotate-[30deg] xl:-right-44 xl:-top-5 xl:z-10" alt="">
+          <img src="<?php the_field('cta_decor', 'option'); ?>" class="w-48 max-lg:mb-16 xl:absolute xl:w-80 xl:rotate-[30deg] xl:-right-44 xl:-top-5 xl:z-10" alt="">
           <div class="w-full xl:w-6/12">
-            <h2 class="text-3xl leading-8 font-medium text-white uppercase mb-12 max-lg:text-center sm:text-5xl sm:leading-[1.1] lg:text-6xl lg:leading-[1.1]">are you ready to plunge into the wonderful world?</h2>
+            <h2 class="text-3xl leading-8 font-medium text-white uppercase mb-12 max-lg:text-center sm:text-5xl sm:leading-[1.1] lg:text-6xl lg:leading-[1.1]"><?php the_field('cta_title', 'option') ?></h2>
             <div class="group flex items-center flex-col gap-y-5 text-white lg:items-start lg:gap-y-9">
-              <div class="text-lg leading-5 max-lg:font-semibold lg:text-3xl">Contacts</div>
-              <p class="max-lg:text-center text-lg leading-5 lg:text-xl lg:leading-6">123 Main Street, City, Country, Postal Code</p>
-              <a href="tel:+3700000000" class="max-lg:text-center text-lg leading-5 hover:text-accent lg:text-xl lg:leading-6">+37 000 000 00</a>
-              <a href="mailto:Info@mail.com" class="max-lg:text-center text-lg leading-5 hover:text-accent lg:text-xl lg:leading-6">Info@mail.com</a>
-              <a href="#" class="inline-flex items-center justify-center gap-x-6 py-6 px-8 text-lg leading-5 font-medium text-white rounded-full border-white border border-opacity-20 backdrop-blur-lg bg-card max-lg:order-1 max-lg:mt-4 lg:text-xl lg:leading-6 hover:shadow-myShadow1">Write<i class="icomoon icon-whatsapp text-2xl leading-none font-normal"></i></a>
+              <div class="text-lg leading-5 max-lg:font-semibold lg:text-3xl"><?php the_field('cta_subtitle', 'option'); ?></div>
+              <p class="max-lg:text-center text-lg leading-5 lg:text-xl lg:leading-6"><?php the_field('address', 'option'); ?></p>
+              <?php $phone = get_field('phone', 'option');
+              if( $phone ): 
+                  $phone_url = $phone['url'];
+                  $phone_title = $phone['title'];
+                  $phone_target = $phone['target'] ? $phone['target'] : '_self';
+                  ?>
+                  <a href="<?php echo esc_url( $phone_url ); ?>" target="<?php echo esc_attr( $phone_target ); ?>" class="max-lg:text-center text-lg leading-5 hover:text-accent lg:text-xl lg:leading-6"><?php echo esc_html( $phone_title ); ?></a>
+              <?php endif; ?>
+              <?php $email = get_field('email', 'option');
+              if( $email ): 
+                  $email_url = $email['url'];
+                  $email_title = $email['title'];
+                  $email_target = $email['target'] ? $email['target'] : '_self';
+                  ?>
+                  <a href="<?php echo esc_url( $email_url ); ?>" target="<?php echo esc_attr( $email_target ); ?>" class="max-lg:text-center text-lg leading-5 hover:text-accent lg:text-xl lg:leading-6"><?php echo esc_html( $email_title ); ?></a>
+              <?php endif; ?>
+              <?php $whatsapp_write = get_field('whatsapp_write', 'option');
+              if( $whatsapp_write ): 
+                  $whatsapp_write_url = $whatsapp_write['url'];
+                  $whatsapp_write_title = $whatsapp_write['title'];
+                  $whatsapp_write_target = $whatsapp_write['target'] ? $whatsapp_write['target'] : '_self';
+                  ?>
+                  <a href="<?php echo esc_url( $whatsapp_write_url ); ?>" target="<?php echo esc_attr( $whatsapp_write_target ); ?>" class="inline-flex items-center justify-center gap-x-6 py-6 px-8 text-lg leading-5 font-medium text-white rounded-full border-white border border-opacity-20 backdrop-blur-lg bg-card max-lg:order-1 max-lg:mt-4 lg:text-xl lg:leading-6 hover:shadow-myShadow1"><?php echo esc_html( $whatsapp_write_title ); ?><i class="icomoon icon-whatsapp text-2xl leading-none font-normal"></i></a>
+              <?php endif; ?>
             </div>
           </div>
           <div class="w-full p-7 text-white border-white border border-opacity-20 backdrop-blur-lg bg-card h-fit lg:p-12 lg:pb-20 xl:w-5/12">
-            <form>
+            <!-- <form>
               <div class="mb-5 lg:mb-6">
                 <label for="name" class="hidden"></label>
                 <input type="text" id="name" class="h-[60px] bg-transparent border border-[#3B4A59] text-white text-lg leading-4 text-opacity-50 outline-none focus:text-opacity-80 focus:border-accent block w-full p-5" placeholder="Name" required>
@@ -439,7 +460,8 @@ get_header(); ?>
                 <button type="submit" class="w-full h-[60px] text-white font-medium text-lg leading-none px-7 py-5 text-center border-white border border-opacity-20 lg:w-auto lg:text-xl lg:leading-none hover:backdrop-blur-lg hover:bg-card">Send</button>
                 <p class="text-white text-sm font-normal opacity-50 max-lg:text-center">by clicking the "submit" button, you agree to the <a href="#" class="underline hover:no-underline">privacy policy</a></p>
               </div>
-            </form>
+            </form> -->
+            <?php echo do_shortcode('[contact-form-7 id="155" title="Form Are you ready"]'); ?>
           </div>
         </div>
       </div>
