21134b5fcddb6d893f2af9e83649b2cef161b555
daneliyan <105802998+daneliyan@users.noreply.github.com>
2023-07-18 20:28:33 +0300

    Добавил страницу 404 и исправил Index.php

---
+++ wp-content/themes/freediving/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+  <section class="hero relative z-0 pt-32 pb-16 mb-24 sm:mb-32 lg:pt-[230px] lg:pb-48">
+    <div class="mx-auto container px-3 sm:px-6 lg:px-16">
+      <h1 class="text-white uppercase font-medium max-w-[1440px] text-3xl mb-7 max-lg:mb-64 md:text-6xl 2xl:text-8xl">Page not found</h1>
+      <div class="grid gap-y-7 text-lg leading-6 text-white lg:text-xl lg:leading-8 lg:mb-14">Page not found or deleted. You can go to the main page, or any other, through the menu.</div>
+      <a href="/" class="inline-flex items-center justify-center gap-x-6 py-6 px-8 text-lg leading-5 font-medium text-white rounded-full border-white border border-opacity-20 backdrop-blur-lg bg-card max-lg:order-1 max-lg:mt-4 lg:text-xl lg:leading-6 hover:shadow-myShadow1">Back to home</a>
+    </div>
+  </div>
+
+
+<?php get_footer(); ?>
